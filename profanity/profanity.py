b7b36a4976470aeeb25a866e583a2f094615d71f
Ben Friedland <bfriedland@tiny13.local>
2013-11-27 11:22:56 -0800

    Basic structure of main file.

--- profanity/profanity.py
+++ profanity/profanity.py
@@ -7,14 +7,13 @@ lines = None
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
+
 def get_data(path):
     return os.path.join(_ROOT, 'data', path)
 
 
-def contains_profanity(input_text): 
+def contains_profanity(input_text):
     """Checks the input_text for any profanity and returns True if it does.
     Otherwise, returns False.
     """
-
-
-
+    return False
