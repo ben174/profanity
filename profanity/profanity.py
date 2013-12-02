84d8ade88670ec63acdbbd7394aa2d8754b618c3
Ben Friedland <ben174@gmail.com>
2013-12-02 08:37:04 -0800

    Adding a profanity character pool which allows you to specify censor characters.

--- profanity/profanity.py
+++ profanity/profanity.py
@@ -2,9 +2,11 @@
 
 import os
 import argparse
+import random
 
 lines = None
 words = None
+_censor_chars = '@#$%!'
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
@@ -19,6 +21,15 @@ def get_words():
     return words
 
 
+def set_censor_characters(censor_chars):
+    """ Sets the pool of censor characters. Input should be a single string
+    containing all the censor charcters you'd like to use. 
+    Example: "@#$%^"
+
+    """
+    _censor_chars = censor_chars 
+
+
 def contains_profanity(input_text):
     """Checks the input_text for any profanity and returns True if it does.
     Otherwise, returns False.
@@ -31,14 +42,23 @@ def contains_profanity(input_text):
 
 
 def censor(input_text):
+    """ Returns the input string with profanity replaced with a random string
+    of characters plucked from the censor_characters pool.
+
+    """
     ret = input_text
     words = get_words()
     for word in words:
-        ret = ret.replace(word, '*'*len(word))
+        cen = "".join([random.choice(list(censor_chars)) for l in list(word)])
+        ret = ret.replace(word, cen)
     return ret
 
 
 def load_words(filename=None):
+    """ Loads and caches the profanity word list. Input file (if provided)
+    should be a flat text file with one profanity entry per line.
+
+    """
     if not filename:
         filename = get_data('wordlist.txt')
     f = open(filename)
@@ -53,6 +73,9 @@ def main():
                         help='Path to input file to check.')
     parser.add_argument('-t', '--text', dest='text', type=str,
                         help='Text to check.')
+    parser.add_argument("--censor", help="Returns censored text.",
+                                action="store_true")
+
     args = parser.parse_args()
     if args.path:
         f = open(args.path)
@@ -62,7 +85,12 @@ def main():
     else:
         print "No input specified."
         return
+
+    if args.censor:
+        print censor(text)
+        return
     print contains_profanity(text)
+    return
 
 
 if __name__ == '__main__':
